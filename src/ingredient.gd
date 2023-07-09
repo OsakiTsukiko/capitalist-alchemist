3f71e7a17a1fd1d418c940586f690f64e0deab9e
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2023-07-09 17:03:10 +0300

    stuff

--- src/ingredient.gd
+++ src/ingredient.gd
@@ -2,16 +2,19 @@ class_name Ingredient
 extends Node
 
 var id: String
+var texture: Texture
+var description: String
 var color: Color
 var prop_arr: Array[int]
 var num: int
 
-func _init(p_id: String, p_color: Color, p_prop_arr: Array[int], p_num: int):
+func _init(p_id: String, p_texture: Texture, p_description: String, p_color: Color, p_prop_arr: Array[int], p_num: int):
 	id = p_id
+	texture = p_texture
+	description = p_description
 	color = p_color
 	prop_arr = p_prop_arr
 	num = p_num
-
 
 enum INGREDIENT_PROPERTIES {
 	MOISTURE = 0,
